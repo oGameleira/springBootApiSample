04a08b9e41c8503f56287a5263dc685d9bc9ce74
Luciano Gameleira <lucianogameleira@gmail.com>
2021-12-09 19:43:13 -0800

    Removing Unused classes

--- src/test/java/com/gameleira/springBootApiSample/HelloControllerTests.java
+++ src/test/java/com/gameleira/springBootApiSample/HelloControllerTests.java
@@ -2,19 +2,12 @@ package com.gameleira.springBootApiSample;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import static org.assertj.core.api.Assertions.assertThat;
 import com.gameleira.springBootApiSample.controllers.HelloController;
-import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.hamcrest.Matchers.containsString;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class HelloControllerTests {
